e19566fca2f045313bcc26f1d3650d29502e26fc
Marconius <marconius.dev@gmail.com>
2021-09-03 21:29:26 +0100

    Update TowerOfHanoi.js
    Mostly learning from articles, changes something so I would have something to commit

--- src/Components/Challenges/TowerOfHanoi/TowerOfHanoi.js
+++ src/Components/Challenges/TowerOfHanoi/TowerOfHanoi.js
@@ -7,9 +7,9 @@ const TowerOfHanoi = () => {
     const [numberOfTowers, setNumberOfTowers] = useState(3);
     const [towers, setTowers] = useState(() => {
         let arr = [];
-        arr.push(<Tower id={1} discs={[1,1,1]}/>);
-        arr.push(<Tower id={2} discs={[0,0,0]}/>);
-        arr.push(<Tower id={3} discs={[0,0,0]}/>);
+        arr.push(<Tower id={1} discs={[1,1,1,1]}/>);
+        arr.push(<Tower id={2} discs={[0,0,0,0]}/>);
+        arr.push(<Tower id={3} discs={[0,0,0,0]}/>);
         // for (let i = 2; i <= numberOfTowers; i++) {
         //     arr.push(<Tower id={i} discs={[0,1,0,1]} />);
         // }
