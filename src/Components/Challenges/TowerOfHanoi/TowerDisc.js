c507937468f2c0176d5d873199fca60631b3bb59
Marconius <marconius.dev@gmail.com>
2021-09-14 23:23:17 +0100

    Disc component updated

--- src/Components/Challenges/TowerOfHanoi/TowerDisc.js
+++ src/Components/Challenges/TowerOfHanoi/TowerDisc.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const TowerDisc = () => {
+const TowerDisc = (props) => {
     return (
         <>
-            <Disc/>
+            <Disc className={props.discClass}/>
         </>
     )
 }
@@ -14,13 +14,33 @@ const Disc = styled.div`
     width: 90px;
     height: 15px;
     z-index: 2;
-    border-radius: 5px;
+    border-radius: 8px;
     border: 1px solid rgb(83, 83, 83);
     background-color: var(--cmarc-blue-2);
     &:hover {
         transform: scale(1.1, 1.1);
         box-shadow: 0 0 5px 1px white;
         transition: 200ms;
+    }
+
+    &.size0 {
+        background-color: var(--cmarc-blue-2);
+        width: 30px;
+    }
+
+    &.size1 {
+        background-color: #c4e941;
+        width: 50px;
+    }
+
+    &.size2 {
+        background-color: #4337b7;
+        width: 70px;
+    }
+
+    &.size3 {
+        background-color: #cf567a;
+        width: 90px;
     }
 `
   
--- src/Components/Challenges/TowerOfHanoi/TowerOfHanoi.js
+++ src/Components/Challenges/TowerOfHanoi/TowerOfHanoi.js
@@ -6,21 +6,32 @@ import './TowerOfHanoi.css'
 const TowerOfHanoi = () => {
     
     const [numberOfTowers, setNumberOfTowers] = useState(3);
+    const [numberOfDiscs, setNumberOfDiscs] = useState(4);
 
     const returnTowerClickedInfo = (towerId) => {
         console.log("Tower " + towerId + " clicked!");
     }
 
-    const [towers, setTowers] = useState(() => {
+    const [discs, setDiscs] = useState(() => {
         let arr = [];
-        arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={1} discs={[1,1,1,1]}/>);
-        arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={2} discs={[0,0,0,0]}/>);
-        arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={3} discs={[0,0,0,0]}/>);
-        // for (let i = 2; i <= numberOfTowers; i++) {
-        //     arr.push(<Tower id={i} discs={[0,1,0,1]} />);
-        // }
+        for (let i = 0; i < numberOfDiscs; i++) {
+            arr.push(<TowerDisc discClass={"size" + i}/>);
+        }
         return arr;
     })
+
+    const [towers, setTowers] = useState(() => {
+        let arr = [];
+        // arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={1} discs={[1,1,1,1]}/>);
+        // arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={2} discs={[0,0,0,0]}/>);
+        // arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={3} discs={[0,0,0,0]}/>);
+        for (let i = 0; i < numberOfTowers; i++) {
+            arr.push(<Tower id={i} discs={[1,1,1,1]} />);
+        }
+        return arr;
+    })
+
+    
     
     return (
         <>
@@ -30,7 +41,7 @@ const TowerOfHanoi = () => {
                     <div className="instructions">
                         <h3>Goal: Move the discs from the left tower over to the right tower.</h3>
                         <h3>Note: Only one disk can be moved at a time. No disk can be placed on top of the smaller disk.</h3>
-                        <TowerDisc/>
+                        {discs.map(disc => {return disc})}
                     </div>
                     
 
