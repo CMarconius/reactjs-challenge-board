78d90a641912743d0d5dcf1e3c7093c503e105af
Marconius <marconius.dev@gmail.com>
2021-09-15 23:01:38 +0100

    Discs cpassed back to parent and working on preventing further removals

--- src/Components/Challenges/TowerOfHanoi/Tower.js
+++ src/Components/Challenges/TowerOfHanoi/Tower.js
@@ -1,82 +1,100 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Tower.css'
 
 const Tower = (props) => {
 
     const [towerActive, setTowerActive] = useState(false)
 
-    const [discCount, setDiscCount] = useState(props.discs.reduce((a, b) => a + b, 0))
+    const [discs, setDiscs] = useState(props.discs)
 
-    const [disc1Present, setDisc1Present] = useState(() => {
-        return props.discs[0] === 1 ? true : false;    
-    });
-    const [disc2Present, setDisc2Present] = useState(() => {
-        return props.discs[1] === 1 ? true : false;    
-    });
-    const [disc3Present, setDisc3Present] = useState(() => {
-        return props.discs[2] === 1 ? true : false;    
-    });
-    const [disc4Present, setDisc4Present] = useState(() => {
-        return props.discs[3] === 1 ? true : false;    
-    });
+    const [discCount, setDiscCount] = useState(discs.length)
 
-    const [disc1Active, setDisc1Active] = useState("");
-    const [disc2Active, setDisc2Active] = useState("");
-    const [disc3Active, setDisc3Active] = useState("");
-    const [disc4Active, setDisc4Active] = useState("");
+
+    // const [discCount, setDiscCount] = useState(props.discs.reduce((a, b) => a + b, 0))
+
+    // const [disc1Present, setDisc1Present] = useState(() => {
+    //     return props.discs[0] === 1 ? true : false;    
+    // });
+    // const [disc2Present, setDisc2Present] = useState(() => {
+    //     return props.discs[1] === 1 ? true : false;    
+    // });
+    // const [disc3Present, setDisc3Present] = useState(() => {
+    //     return props.discs[2] === 1 ? true : false;    
+    // });
+    // const [disc4Present, setDisc4Present] = useState(() => {
+    //     return props.discs[3] === 1 ? true : false;    
+    // });
+
+    // const [disc1Active, setDisc1Active] = useState("");
+    // const [disc2Active, setDisc2Active] = useState("");
+    // const [disc3Active, setDisc3Active] = useState("");
+    // const [disc4Active, setDisc4Active] = useState("");
+
+    
 
     const handleTowerClicked = () => {
-        setTowerActive(true)
-        props.returnTowerClickedInfo(props.id);
+        console.log("Yo, it's " + props.discActive)
+        if (!(props.discActive)) {
+            if (discs.length > 0) {
+                props.returnTowerClickedInfo(discs[0]);
+                discs.shift()
+                setDiscCount(discs.length)
+            }
+        }
     }
 
-    const handleClick1 = () => {
-        if (disc1Active === "") {
-            setDisc1Active("active")
-        }
-        else {
-            setDisc1Active("")
-        }
-        setDisc2Active("")
-        setDisc3Active("")
-        setDisc4Active("")
-    }
+    // const handleClick1 = () => {
+    //     if (disc1Active === "") {
+    //         setDisc1Active("active")
+    //     }
+    //     else {
+    //         setDisc1Active("")
+    //     }
+    //     setDisc2Active("")
+    //     setDisc3Active("")
+    //     setDisc4Active("")
+    // }
 
-    const handleClick2 = () => {
-        setDisc1Active("")
-        if (disc2Active === "") {
-            setDisc2Active("active")
-        }
-        else {
-            setDisc2Active("")
-        }
-        setDisc3Active("")
-        setDisc4Active("")
-    }
+    // const handleClick2 = () => {
+    //     setDisc1Active("")
+    //     if (disc2Active === "") {
+    //         setDisc2Active("active")
+    //     }
+    //     else {
+    //         setDisc2Active("")
+    //     }
+    //     setDisc3Active("")
+    //     setDisc4Active("")
+    // }
 
-    const handleClick3 = () => {
-        setDisc1Active("")
-        setDisc2Active("")
-        if (disc3Active === "") {
-            setDisc3Active("active")
-        }
-        else {
-            setDisc3Active("")
-        }
-        setDisc4Active("")
-    }
+    // const handleClick3 = () => {
+    //     setDisc1Active("")
+    //     setDisc2Active("")
+    //     if (disc3Active === "") {
+    //         setDisc3Active("active")
+    //     }
+    //     else {
+    //         setDisc3Active("")
+    //     }
+    //     setDisc4Active("")
+    // }
 
-    const handleClick4 = () => {
-        setDisc1Active("")
-        setDisc2Active("")
-        setDisc3Active("")
-        if (disc4Active === "") {
-            setDisc4Active("active")
-        }
-        else {
-            setDisc4Active("")
-        }
-    }
+    // const handleClick4 = () => {
+    //     setDisc1Active("")
+    //     setDisc2Active("")
+    //     setDisc3Active("")
+    //     if (disc4Active === "") {
+    //         setDisc4Active("active")
+    //     }
+    //     else {
+    //         setDisc4Active("")
+    //     }
+    // }
+
+
+    useEffect(() => {
+
+    }, [discs]);
 
 
     return (
@@ -86,10 +104,12 @@ const Tower = (props) => {
                 <div className="towerSection">
                     <div className="towerPole"></div>
                     <div className={"discsSection discCount" + discCount}>
-                        {disc1Present ? <div onClick={handleClick1} className={"towerDisc disc1 " + disc1Active}></div> : null}
+                        {discs.map(disc => {return disc})}
+
+                        {/* {disc1Present ? <div onClick={handleClick1} className={"towerDisc disc1 " + disc1Active}></div> : null}
                         {disc2Present ? <div onClick={handleClick2} className={"towerDisc disc2 " + disc2Active}></div> : null}
                         {disc3Present ? <div onClick={handleClick3} className={"towerDisc disc3 " + disc3Active}></div> : null}
-                        {disc4Present ? <div onClick={handleClick4} className={"towerDisc disc4 " + disc4Active}></div> : null}
+                        {disc4Present ? <div onClick={handleClick4} className={"towerDisc disc4 " + disc4Active}></div> : null} */}
                     </div>
                 </div>
                 <div className="towerBase"></div>
--- src/Components/Challenges/TowerOfHanoi/TowerOfHanoi.js
+++ src/Components/Challenges/TowerOfHanoi/TowerOfHanoi.js
@@ -1,21 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Tower from './Tower';
 import TowerDisc from './TowerDisc';
 import './TowerOfHanoi.css'
 
 const TowerOfHanoi = () => {
     
-    const [numberOfTowers, setNumberOfTowers] = useState(3);
-    const [numberOfDiscs, setNumberOfDiscs] = useState(4);
+    const [numberOfTowers, setNumberOfTowers] = useState(3)
+    const [numberOfDiscs, setNumberOfDiscs] = useState(4)
 
-    const returnTowerClickedInfo = (towerId) => {
-        console.log("Tower " + towerId + " clicked!");
+    const [currentDisc, setCurrentDisc] = useState()
+    const [discActive, setDiscActive] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('keydown', handleMouseMove);
+        };
+    }, [handleMouseMove]);
+
+    useEffect(() => {
+       
+    }, [discActive]);
+    
+
+    function handleMouseMove() {
+        const root = document.documentElement;
+        
+        document.addEventListener('mousemove', evt => {
+            let x = evt.clientX;
+            let y = evt.clientY;
+        
+            root.style.setProperty('--mouse-x', x);
+            root.style.setProperty('--mouse-y', y);
+        });
+    }
+
+    const returnTowerClickedInfo = (clickedDisc) => {
+        // console.log("Tower " + towerId + " clicked!");
+        console.log(clickedDisc)
+        setDiscActive(true)
+        setCurrentDisc(clickedDisc)
     }
 
     const [discs, setDiscs] = useState(() => {
         let arr = [];
         for (let i = 0; i < numberOfDiscs; i++) {
-            arr.push(<TowerDisc discClass={"size" + i}/>);
+            arr.push(<TowerDisc discClass={"size" + i}/>)
         }
         return arr;
     })
@@ -25,11 +56,18 @@ const TowerOfHanoi = () => {
         // arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={1} discs={[1,1,1,1]}/>);
         // arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={2} discs={[0,0,0,0]}/>);
         // arr.push(<Tower returnTowerClickedInfo={returnTowerClickedInfo} id={3} discs={[0,0,0,0]}/>);
-        for (let i = 0; i < numberOfTowers; i++) {
-            arr.push(<Tower id={i} discs={[1,1,1,1]} />);
+
+        arr.push(<Tower discActive={discActive} returnTowerClickedInfo={returnTowerClickedInfo} id={1} discs={[...discs]} />);
+
+        for (let i = 2; i <= numberOfTowers; i++) {
+            arr.push(<Tower discActive={discActive} returnTowerClickedInfo={returnTowerClickedInfo} id={i} discs={[]}/>);
         }
         return arr;
     })
+    
+    function getDiscActive() {
+        return discActive
+    }
 
     
     
@@ -41,7 +79,13 @@ const TowerOfHanoi = () => {
                     <div className="instructions">
                         <h3>Goal: Move the discs from the left tower over to the right tower.</h3>
                         <h3>Note: Only one disk can be moved at a time. No disk can be placed on top of the smaller disk.</h3>
-                        {discs.map(disc => {return disc})}
+                    </div>
+
+                    <div className="currentDisc">
+                        <h3>Currnet Disc</h3>
+                        <div className="currentDiscContainer">
+                            {currentDisc}
+                        </div>
                     </div>
                     
 
