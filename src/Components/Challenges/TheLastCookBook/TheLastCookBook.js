df55a3247ce67e0cf23c4e30216d61eaae5d7d97
Marconius <marconius.dev@gmail.com>
2021-09-28 17:26:09 +0100

    Cookbook - deciding on styling

--- src/Components/Challenges/TheLastCookBook/TheLastCookBook.js
+++ src/Components/Challenges/TheLastCookBook/TheLastCookBook.js
@@ -3,7 +3,9 @@ import { Button } from '../../Button'
 import styled from 'styled-components'
 
 const TheLastCookBook = () => {
-    const [myIngredients, setMyIngredients] = useState(["flour", "beans"])
+    const [cookBookState, setCookBookState] = useState("d")
+
+    const [myIngredients, setMyIngredients] = useState(["Flour", "Beans"])
     const [currentIngredient, setCurrentIngredient] = useState("")
 
     const handleIngredientChange = (e) => {
@@ -11,8 +13,12 @@ const TheLastCookBook = () => {
     }
 
     const AddIngredient = () => {
-        setMyIngredients([...myIngredients, currentIngredient]);
-        setCurrentIngredient("")
+        let ingr = currentIngredient.toLowerCase()
+        ingr = ingr.charAt(0).toUpperCase() + ingr.slice(1)
+        if (!myIngredients.includes(ingr)) {
+            setMyIngredients([...myIngredients, ingr]);
+            setCurrentIngredient("")
+        }
     }
 
     useEffect(() => {
@@ -23,14 +29,26 @@ const TheLastCookBook = () => {
         <>
         <CookBook>
             <h1>The Last Cookbook You Will Ever Need</h1>
-            <input onChange={handleIngredientChange} type="text" placeholder="Add an ingredient..." value={currentIngredient}/>
-            <Button buttonSize={"btn--small"}  buttonActive={true} onClick={AddIngredient}>Add New Ingredient</Button>
-            <MyListOfIngredients>
-                <h2>Here's my List of Ingredients:</h2>
-                {myIngredients.map(ingredient => {
-                    return <h3>{ingredient}</h3>
-                })}
-            </MyListOfIngredients>
+
+            {(cookBookState==="addIngredient") ? ( 
+                <>
+                    <input onChange={handleIngredientChange} type="text" placeholder="Add an ingredient..." value={currentIngredient}/>
+                    <Button buttonSize={"btn--small"}  buttonActive={true} onClick={AddIngredient}>Add New Ingredient</Button>
+                    <MyListOfIngredients>
+                        <h2>Here's my List of Ingredients:</h2>
+                        {myIngredients.map(ingredient => {
+                            return <Ingredient>{ingredient}</Ingredient>
+                        })}
+                    </MyListOfIngredients>
+                </>
+            ) : (
+                <>
+                    <h2>Select Recipe</h2>
+                    <Button buttonSize={"btn--small"}  buttonActive={true} onClick={() => setCookBookState("addIngredient")}>Filter Recipes by Ingredient</Button>
+                </>
+            )
+            }
+
         </CookBook>
         </>
     )
@@ -40,7 +58,7 @@ export default TheLastCookBook
 
 const CookBook = styled.div`
     max-width: 100%;
-    background-color: #c2c2c2;
+    background-color: #f0f0f0;
     margin: 15px;
     padding: 15px;  
     border-radius: 20px;
@@ -48,6 +66,9 @@ const CookBook = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    input {
+        box-shadow: 0px 0px 4px #888888;
+    }
 `
 
 const MyListOfIngredients = styled.div`
@@ -56,18 +77,22 @@ const MyListOfIngredients = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    color: white;
+    color: black;
     margin-left: auto;
     margin-right: auto;
     padding: 20px 10px;
     border-radius: 20px;
-    background-color: var(--cmarc-blue-3);
+    border: solid 2px  var(--tlcb-5);
+    background-color: var(--tlcb-1);
     h2 {
-        border-bottom: solid 1px white;
+        border-bottom: solid 1px var(--tlcb-2);
         margin-bottom: 10px;
     }
-    h3 {
-        font-weight: 100;
-    }
-    
+`
+
+const Ingredient = styled.li`
+    font-weight: 700;
+    border-bottom: dotted 3px var(--tlcb-2);
+    width: 60%;
+    margin-bottom: 5px;
 `
